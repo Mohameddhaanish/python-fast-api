b304e28c40302531f93e6f5a18a0e6ba1d5f31b3
Mohameddhaanish <52352219+Mohameddhaanish@users.noreply.github.com>
2025-05-17 23:57:01 +0530

    encryption

--- app/core/config.py
+++ app/core/config.py
@@ -10,5 +10,6 @@ class Settings:
     ALGORITHM:str=os.getenv("ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
     GOOGLE_APP_KEY:str=os.getenv("GOOGLE_APP_KEY")
-
+    CRYPTO_JS_SECRET:str=os.getenv("CRYPTO_JS_SECRET")
+    
 settings = Settings()
